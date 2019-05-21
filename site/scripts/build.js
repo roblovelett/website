e12b4370ffaf88261a76b81b97130aff861dddbb
Rob Lovelett <rlovelett@gmail.com>
2019-05-21 13:20:27 -0500

    build script pulls width of images in content/**/*.png,jpg,gif, pushes to indv. image obj width property, appends remaining image widths from imageOptions widths property.

--- site/scripts/build.js
+++ site/scripts/build.js
@@ -4,14 +4,37 @@ var fs = require('fs'),
     glob = require('glob'),
     serveStatic = require('serve-static');
 
-var imagePaths = glob.sync("../content/**/*.{png,jpg,gif}");
+var imageOptions = {
+        widths: [3840, 2560, 1920, 1366, 1280, 640, 320, 160],
+    },
+    imagePaths = glob.sync("../content/**/*.{png,jpg,gif}");
 
 if (Array.isArray(imagePaths) && imagePaths.length) {
-    // do this
-}
+    var image = {},
+        images;
+    
+    for (i=0; i < imagePaths.length; i++) {
+        image.path = imagePaths[i];
+        image.widths = [];
 
-options = {
-    widths: [3840, 2560, 1920, 1366, 1280, 640, 320, 160],
-}
+        // get original width
+        gm(image.path).identify((err, data) => {
+            if (!err) {
+                image.widths.push(data.size.width);        
+                
+                for (i=0; i < imageOptions.widths.length; i++) {
+                    if (imageOptions.widths[i] < image.widths[0]) {
+                        console.log(imageOptions.widths[i] + ' is < ' + image.widths[0]);
+                        image.widths.push(imageOptions.widths[i]);
+                   }
+                }
 
-var images = [];
+                console.log('image widths:' + image.widths);
+            }
+        });
+
+        // generate images below original width
+        // image.widths;
+
+    }
+}
